e52dfeb053e8b0d35244690d26f75f1a1c9c1f55
miguel <migue1990@gmail.com>
2015-03-04 15:01:51 -0700

    initial commit

---
+++ src/main/java/com/autocasa/AutocasaApplication.java
@@ -0,0 +1,12 @@
+package com.autocasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocasaApplication.class, args);
+    }
+}
