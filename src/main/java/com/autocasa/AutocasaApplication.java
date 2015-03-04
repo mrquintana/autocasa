18e56e22502cf11f292cff0515dfd5b6bb9b6d23
miguel <migue1990@gmail.com>
2015-03-04 16:11:17 -0700

    removed port config in main

--- src/main/java/com/autocasa/AutocasaApplication.java
+++ src/main/java/com/autocasa/AutocasaApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutocasaApplication {
 
     public static void main(String[] args) {
-    	String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
         System.setProperty("server.port", webPort);
         SpringApplication.run(AutocasaApplication.class, args);
     }
